2719fbb88a8f696343d757ae81da23f253f1f548
Kaki Ryan <kakiryan@cs.unc.edu>
2021-12-16 16:12:56 -0500

    progress on new continuous assign solution and counting number of paths to explore as i go

--- engine.py
+++ engine.py
@@ -2,13 +2,14 @@ import pyverilog
 import z3
 from z3 import Solver, Int, BitVec, Context, BitVecSort, ExprRef, BitVecRef, If, BitVecVal
 from pyverilog.vparser.parser import parse
-from pyverilog.vparser.ast import Description, ModuleDef, Node, IfStatement, SingleStatement, And, Constant, Rvalue, Plus
-from pyverilog.vparser.ast import WhileStatement, ForStatement, CaseStatement, Block, SystemCall, Land, InstanceList, IntConst
+from pyverilog.vparser.ast import Description, ModuleDef, Node, IfStatement, SingleStatement, And, Constant, Rvalue, Plus, Input, Output
+from pyverilog.vparser.ast import WhileStatement, ForStatement, CaseStatement, Block, SystemCall, Land, InstanceList, IntConst, Partselect, Ioport
 from pyverilog.vparser.ast import Value, Reg, Initial, Eq, Identifier, Initial,  NonblockingSubstitution, Decl, Always, Assign, NotEql
 import sys
 import os
 from optparse import OptionParser
 from typing import Optional
+import random, string
 import time
 
 
@@ -40,6 +41,11 @@ class ExecutionManager:
     assertion_violation: bool = False
     in_always: bool = False
     modules = {}
+    dependencies = {}
+    updates = {}
+    seen = []
+    final = False
+    completed = []
 
 def to_binary(i: int, digits: int = 32) -> str:
     num: str = bin(i)[2:]
@@ -90,8 +96,9 @@ def parse_expr_to_Z3(e: Value, s: Solver, branch: bool):
 
 
 def parse_rvalue(rvalue: Rvalue, store) -> (str, str, str): 
+    #print(rvalue)
     tokens = str(rvalue).replace('(','').replace(')','').split()
-    print(tokens)
+    #print(tokens)
     op = ""
     if 'Plus' in tokens[0]:
         op = "+"
@@ -100,10 +107,21 @@ def parse_rvalue(rvalue: Rvalue, store) -> (str, str, str):
     lhs = tokens[1]
     rhs = tokens[2]
     if not lhs.isdigit():
+        if lhs == "Plus":
+            lhs = tokens[2]
+            rhs = tokens[3]
         lhs = store[lhs]
     if not rhs.isdigit():
+        # print(store)
+        # print(rvalue)
         rhs = store[rhs]
     return (lhs, op, rhs)
+
+
+def init_symbol() -> str:
+    """Initializes signal with random symbol."""
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    
      
 class ExecutionEngine:
     branch: bool = True
@@ -121,6 +139,14 @@ class ExecutionEngine:
             s.pop()
             return False
 
+    def check_dup(self, m: ExecutionManager) -> bool:
+        """Checks if the current path is a duplicate/worth exploring."""
+        print(m.completed)
+        for i in range(len(m.path_code)):
+            if m.path_code[i] == "1" and i in m.completed:
+                return True
+        return False
+
     def solve_pc(self, s: Solver) -> None:
         """Solve path condition."""
         result = str(s.check())
@@ -129,6 +155,31 @@ class ExecutionEngine:
             print(model)
         else:
             print("UNSAT/FAILED")
+
+    def count_conditionals_2(self, m:ExecutionManager, items) -> int:
+        """Rewrite to actually return an int."""
+        stmts = items
+        if isinstance(items, Block):
+            stmts = items.statements
+            items.cname = "Block"
+
+        if hasattr(stmts, '__iter__'):
+            for item in stmts:
+                if isinstance(item, CONDITIONALS):
+                    if isinstance(item, IfStatement) or isinstance(item, CaseStatement):
+                        if isinstance(item, IfStatement):
+                            return self.count_conditionals_2(m, item.true_statement) + self.count_conditionals_2(m, item.false_statement)  + 1
+                if isinstance(item, Block):
+                    return self.count_conditionals_2(m, item.items)
+                elif isinstance(item, Always):
+                   return self.count_conditionals_2(m, item.statement)             
+                elif isinstance(item, Initial):
+                    return self.count_conditionals_2(m, item.statement)
+        elif items != None:
+            if isinstance(items, IfStatement):
+                return  ( self.count_conditionals_2(m, items.true_statement) + 
+                self.count_conditionals_2(m, items.false_statement)) + 1
+        return 0
 
     def count_conditionals(self, m: ExecutionManager, items):
         """Identify control flow structures to count total number of paths."""
@@ -139,10 +190,11 @@ class ExecutionEngine:
         if hasattr(stmts, '__iter__'):
             for item in stmts:
                 if isinstance(item, CONDITIONALS):
-                    if isinstance(item, IfStatement):
+                    if isinstance(item, IfStatement) or isinstance(item, CaseStatement):
                         m.num_paths *= 2
-                        self.count_conditionals(m, item.true_statement)
-                        self.count_conditionals(m, item.false_statement)
+                        if isinstance(item, IfStatement):
+                            self.count_conditionals(m, item.true_statement)
+                            self.count_conditionals(m, item.false_statement)
                 if isinstance(item, Block):
                     self.count_conditionals(m, item.items)
                 elif isinstance(item, Always):
@@ -155,16 +207,40 @@ class ExecutionEngine:
                 self.count_conditionals(m, items.true_statement)
                 self.count_conditionals(m, items.false_statement)
 
+
+    def seen_all_cases(self, m: ExecutionManager, bit_index: int, nested_ifs: int) -> bool:
+        """Checks if we've seen all the cases for this index in the bit string.
+        We know there are no more nested conditionals within the block, just want to check 
+        that we have seen the path where this bit was turned on but the thing to the left of it
+        could vary."""
+        print(f"bit index: {bit_index}")
+
+        # first check if things less than me have been added.
+        # so index 29 shouldnt be completed before 30
+        for i in range(bit_index + 1, 32):
+            if not i in m.completed:
+                return False
+        count = 0
+        seen = m.seen
+        for path in seen:
+            if path[bit_index ] == '1':
+                count += 1
+        print(f"count: {count}")
+        if count >=  nested_ifs:
+            return True
+        return False
+        
+
     def init_run(self, m: ExecutionManager, module: ModuleDef) -> None:
         """Initalize run."""
         self.count_conditionals(m, module.items)
 
     def visit_expr(self, m: ExecutionManager, s: SymbolicState, expr: Value) -> None:
         if isinstance(expr, Reg):
-            s.store[expr.name] = expr.name + "_0"
+            s.store[expr.name] =''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
         elif isinstance(expr, Eq):
             # assume left is identifier
-            x = BitVec(expr.left.name, 32)
+            x = BitVec(s.store[expr.left.name], 32)
             # assume right is a value
             y = BitVec(int(expr.right.value), 32)
             if self.branch:
@@ -174,7 +250,7 @@ class ExecutionEngine:
         elif isinstance(expr, Identifier):
             # change this to one since inst is supposed to just be 1 bit width
             # and the identifier class actually doesn't have a width param
-            x = BitVec(expr.name, 1)
+            x = BitVec(s.store[expr.name], 1)
             y = BitVec(1, 1)
             if self.branch:
                 s.pc.add(x == y)
@@ -196,6 +272,7 @@ class ExecutionEngine:
         return None
 
     def visit_stmt(self, m: ExecutionManager, s: SymbolicState, stmt: Node):
+        #print(type(stmt))
         if isinstance(stmt, Decl):
             for item in stmt.list:
                 if isinstance(item, Value):
@@ -213,9 +290,19 @@ class ExecutionEngine:
             sub_stmt = stmt.statement
             m.in_always = True
             self.visit_stmt(m, s, sub_stmt)
+            for signal in m.dependencies:
+                if m.dependencies[signal] in m.updates:
+                    if m.updates[m.dependencies[signal]][0] == 1:
+                        prev_symbol = m.updates[m.dependencies[signal]][1]
+                        new_symbol = s.store[m.dependencies[signal]]
+                        s.store[signal] = s.store[signal].replace(prev_symbol, new_symbol)
         elif isinstance(stmt, Assign):
             if isinstance(stmt.right.var, IntConst):
                 s.store[stmt.left.var.name] = stmt.right.var.value
+            elif isinstance(stmt.right.var, Partselect):
+                s.store[stmt.left.var.name] = f"{s.store[stmt.right.var.var.name]}[{stmt.right.var.msb}:{stmt.right.var.lsb}]"
+                m.dependencies[stmt.left.var.name] = stmt.right.var.var.name
+                m.updates[stmt.left.var.name] = 0
             else:
                 (lhs, op, rhs) = parse_rvalue(stmt.right.var, s.store)
                 if (lhs, op, rhs) != ("","",""):
@@ -223,6 +310,7 @@ class ExecutionEngine:
                 else:
                     s.store[stmt.left.var.name] = s.store[stmt.right.var.name]
         elif isinstance(stmt, NonblockingSubstitution):
+            prev_symbol = s.store[stmt.left.var.name]
             if isinstance(stmt.right.var, IntConst):
                 s.store[stmt.left.var.name] = stmt.right.var.value
             else:
@@ -231,6 +319,7 @@ class ExecutionEngine:
                     s.store[stmt.left.var.name] = lhs + op + rhs
                 else:
                     s.store[stmt.left.var.name] = s.store[stmt.right.var.name]
+            m.updates[stmt.left.var.name] = (1, prev_symbol)
         elif isinstance(stmt, Block):
             for item in stmt.statements: 
                 self.visit_stmt(m, s, item)
@@ -241,20 +330,36 @@ class ExecutionEngine:
             # print(stmt.__dict__)
             m.curr_level += 1
             self.cond = True
+            bit_index = len(m.path_code) - m.curr_level
+
             if (m.path_code[len(m.path_code) - m.curr_level] == '1'):
                 self.branch = True
+
+                # check if i am the final thing/no more conditionals after me
+                # basically, we never want me to be true again after this bc we are wasting time reexploring this
+
                 self.visit_expr(m, s, stmt.cond)
                 if (m.abandon):
                     m.abandon = False
+                    print("Abandoning this path!")
                     return
+                nested_ifs = self.count_conditionals_2(m, stmt.true_statement)
+                print(f"Nested ifs {nested_ifs}")
+                diff = 32 - bit_index
+                print(diff)
+                print(m.seen)
+                # m.curr_level == (32 - bit_index) this is always true
+                #if nested_ifs == 0 and m.curr_level < 2 and self.seen_all_cases(m, bit_index, nested_ifs):
+                if self.seen_all_cases(m, bit_index, nested_ifs):
+                    m.completed.append(bit_index)
                 self.visit_stmt(m, s, stmt.true_statement)
             else:
                 self.branch = False
                 self.visit_expr(m, s, stmt.cond)
                 if (m.abandon):
+                    print("Abandoning this path!")
                     m.abandon = False
                     return
-
                 self.visit_stmt(m, s, stmt.false_statement)
         elif isinstance(stmt, SystemCall):
             m.assertion_violation = True
@@ -268,11 +373,24 @@ class ExecutionEngine:
     def visit_module(self, m: ExecutionManager, s: SymbolicState, module: ModuleDef):
         """Visit module."""
         m.currLevel = 0
+        print(m.path_code)
+        params = module.paramlist.params
+        ports = module.portlist.ports
+        for port in ports:
+            if isinstance(port, Ioport):
+                s.store[port.first.name] = init_symbol()
+            else:
+                s.store[port.name] = init_symbol()
+
         for item in module.items:
             if isinstance(item, Value):
-                self.visit_expr()
+                self.visit_expr(m, s, item)
             else:
                 self.visit_stmt(m, s, item)
+        print("Final state:")
+        print(s.store)
+        print("Final path condition:")
+        print(s.pc)
         
             
     def execute(self, ast: ModuleDef, modules, manager: Optional[ExecutionManager]) -> None:
@@ -288,15 +406,26 @@ class ExecutionEngine:
             manager.modules = modules_dict
         self.init_run(manager, ast)
         #print(f"Num paths: {manager.num_paths}")
-        print(f"Num paths {manager.num_paths}")
+        print(f"Upper bound on num paths {manager.num_paths}")
+        manager.seen = []
         for i in range(manager.num_paths):
             manager.path_code = to_binary(i)
+            if self.check_dup(manager):
+            #if False:
+                continue
+            else:
+                print("------------------------")
+                print(f"Path {i}")
             self.visit_module(manager, state, ast)
+            manager.seen.append(manager.path_code)
+            print(manager.updates)
+            print(manager.completed)
             if (manager.assertion_violation):
                 print("Assertion violation")
                 manager.assertion_violation = False
                 self.solve_pc(state.pc)
             manager.curr_level = 0
+            manager.dependencies = {}
             state.pc.reset()
         #manager.path_code = to_binary(0)
         print(f" finishing {ast.name}")
@@ -327,7 +456,6 @@ class ExecutionEngine:
         self.module_depth -= 1
         ## print(state.store)
     
-
 
 def showVersion():
     print(INFO)
---
+++ test_2.v
@@ -0,0 +1,41 @@
+module place_holder (
+  input  CLK,
+  input  RST,
+  output out
+);
+  
+  wire out_wire; 
+  
+  always @(posedge CLK) begin
+    if (RST) begin
+      out <= 0;
+    end
+    else begin
+      out <= out + 1 + out_wire;
+    end
+  end
+  
+  place_holder_2 test_1(
+    .CLK (CLK),
+    .RST (RST),
+    .out (out_wire)
+  );
+  
+endmodule
+
+module place_holder_2 (
+  input  CLK,
+  input  RST,
+  output out
+);
+  
+  always @(posedge CLK) begin
+    if (RST) begin
+      out <= 0;
+    end
+    else begin
+      out <= out + 2;
+    end
+  end
+    
+endmodule
