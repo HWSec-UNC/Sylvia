9f9c23c853d39cb4731bac82822ac4cc0e52b87f
maurille2020 <maurille2020@gmail.com>
2022-07-27 06:35:52 -0400

    Added assertions

--- testing/updowncounter_test.py
+++ testing/updowncounter_test.py
@@ -5,7 +5,10 @@ def run_tests(initial_symbolic_state, final_symbolic_state, pc):
     print("hello")
     print(final_symbolic_state)
     print(pc)
+    assert initial_symbolic_state["updowncounter"]["clock"] != final_symbolic_state["updowncounter"]["clock"]
+    assert initial_symbolic_state["updowncounter"]["reset"] != 1
+    assert initial_symbolic_state["updowncounter"]["value"] == final_symbolic_state["updowncounter"]["value"]
 
 
 #TODO: write the unit test to check that the clocks are the same
-# TODO: write the calls to the test functions 
+#TODO: write the calls to the test functions
