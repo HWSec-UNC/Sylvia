4312d0f5fcd624087f996741bd1a1669ac42ad5e
Kaki Ryan <kakiryan6@gmail.com>
2022-10-10 01:29:55 -0400

    more edits to how edges constructed

--- engine/cfg.py
+++ engine/cfg.py
@@ -1,4 +1,5 @@
 """Extracting the CFG from the AST."""
+from operator import indexOf
 import z3
 from z3 import Solver, Int, BitVec, Context, BitVecSort, ExprRef, BitVecRef, If, BitVecVal, And
 from pyverilog.vparser.parser import parse
@@ -33,13 +34,17 @@ class CFG:
     all_nodes = []
 
     # partition indices
-    partition_points = [0]
+    partition_points = set()
+    partition_points.add(0)
 
     # basic blocks. A list made up of slices of all_nodes determined by partition_points.
     basic_blocks = []
 
-    # the edgelist will be a list of tuples of indices of the basic blocks
+    # the edgelist will be a list of tuples of indices of the ast nodes blocks
     edgelist = []
+
+    # edges between basic blocks, determined by the above edgelist
+    cfg_edges = []
     
     def get_always(self, m: ExecutionManager, s: SymbolicState, ast):
         """get always block"""
@@ -90,18 +95,20 @@ class CFG:
         if hasattr(ast, '__iter__'):
             if isinstance(ast[0], IfStatement):
                 self.all_nodes.append(ast[0])
-                self.partition_points.append(self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 parent_idx = self.curr_idx
                 self.curr_idx += 1
                 edge_1 = (parent_idx, self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.basic_blocks(m, s, ast[0].true_statement) 
                 edge_2 = (parent_idx, self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.basic_blocks(m, s, ast[0].false_statement)
                 self.edgelist.append(edge_1)
                 self.edgelist.append(edge_2)
             elif isinstance(ast[0], CaseStatement):
                 self.all_nodes.append(ast)
-                self.partition_points.append(self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.curr_idx += 1
                 self.basic_blocks(m, s, ast[0].caselist) 
             elif isinstance(ast[0], ForStatement):
@@ -110,8 +117,8 @@ class CFG:
                 self.curr_idx += 1
                 self.basic_blocks(m, s, ast[0].statement) 
             elif isinstance(ast[0], Block):
-                self.all_nodes.append(ast[0])
-                self.curr_idx += 1
+                #self.all_nodes.append(ast[0])
+                #self.curr_idx += 1
                 self.basic_blocks(m, s, ast[0].items)
             elif isinstance(ast[0], Always):
                 # print("entering always")
@@ -128,29 +135,31 @@ class CFG:
                 self.all_nodes.append(ast[0])
         elif ast != None:
             if isinstance(ast, IfStatement):
-                self.partition_points.append(self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.all_nodes.append(ast)
                 parent_idx = self.curr_idx
                 self.curr_idx += 1
                 edge_1 = (parent_idx, self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.basic_blocks(m, s, ast.true_statement) 
                 edge_2 = (parent_idx, self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.basic_blocks(m, s, ast.false_statement)
                 self.edgelist.append(edge_1)
                 self.edgelist.append(edge_2)
             elif isinstance(ast, CaseStatement):
                 self.all_nodes.append(ast)
-                self.partition_points.append(self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.curr_idx += 1
                 self.basic_blocks(m, s, ast.caselist)
             elif isinstance(ast, ForStatement):
                 self.all_nodes.append(ast)
-                self.partition_points.append(self.curr_idx)
+                self.partition_points.add(self.curr_idx)
                 self.curr_idx += 1
                 self.basic_blocks(m, s, ast.statement) 
             elif isinstance(ast, Block):
-                self.all_nodes.append(ast)
-                self.curr_idx += 1
+                #self.all_nodes.append(ast)
+                #self.curr_idx += 1
                 self.basic_blocks(m, s, ast.statements)
             elif isinstance(ast, Always):
                 self.all_nodes.append(ast)
@@ -166,17 +175,31 @@ class CFG:
 
     def partition(self):
         """Slices up the list of all nodes into the actual basic blocks"""
-        self.partition_points.append(len(self.all_nodes)-1)
-
-        for i in range(len(self.partition_points)):
-            if i == len(self.partition_points) - 1: 
-                basic_block = self.all_nodes[self.partition_points[i]]
+        self.partition_points.add(len(self.all_nodes)-1)
+        partition_list = list(self.partition_points)
+        print(partition_list)
+        for i in range(len(partition_list)):
+            if i == len(partition_list) - 1: 
+                basic_block = self.all_nodes[partition_list[i]]
+                self.basic_block_list.append(basic_block)
+            elif i > 0: 
+                basic_block = self.all_nodes[partition_list[i]+1:partition_list[i+1]+1]
                 self.basic_block_list.append(basic_block)
             else:
-                basic_block = self.all_nodes[self.partition_points[i]:self.partition_points[i+1]]
+                basic_block = self.all_nodes[partition_list[i]:partition_list[i+1]+1]
                 self.basic_block_list.append(basic_block)
             print(f" {basic_block} {i}")
 
+    def find_basic_block(self, node_idx: int):
+        """Given a node index, find the basic block that we're in."""
+
+    def make_paths(self):
+        """Map the edge between AST nodes to a path between basic blocks."""
+        for edge in self.edgelist:
+            block1 = self.find_basic_block(edge[0])
+            block2 = self.find_basic_block(edge[1])
+            path = (block1, block2)
+            self.cfg_edges.append(path)
 
 
     def build_cfg(self, m: ExecutionManager, s: SymbolicState):
