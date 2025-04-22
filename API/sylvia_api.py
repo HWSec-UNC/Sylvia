a07e14b701eeda8b997eac8282e48979c35355cd
William Millen <157399497+wvmillen@users.noreply.github.com>
2025-04-22 14:22:14 -0400

    timeout fix attempt

--- API/sylvia_api.py
+++ API/sylvia_api.py
@@ -6,7 +6,7 @@ import json
 import shutil
 import uuid
 """
-run with this one -> 'uvicorn API.sylvia_api:app --host 127.0.0.1 --port 8001` and open on `http://localhost:8001/docs`
+run with this one -> 'uvicorn API.sylvia_api:app --host 127.0.0.1 --port 8001 --timeout-keep-alive 0` and open on `http://localhost:8001/docs`
 """
 
 app = FastAPI()
